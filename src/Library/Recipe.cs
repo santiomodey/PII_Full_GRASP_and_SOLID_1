79d85ee2d42c5e716de0a1be24f32281d32fbe18
Nicolas Texidor <NICOLAS.TEXIDOR@correo.ucu.edu.uy>
2020-10-23 14:31:56 -0300

    Finalizado ejercicio y agregado comentarios

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -6,40 +6,48 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Full_GRASP_And_SOLID.Library
 {
     public class Recipe
     {
-        private ArrayList steps = new ArrayList();
+        //Se cambió para que concuerde con OCP explicado en BaseStep
+        private IList<BaseStep> steps = new List<BaseStep>();
 
         public Product FinalProduct { get; set; }
-
-        public void AddStep(Step step)
+        //Se cambió para que concuerde con OCP explicado en BaseStep
+        public void AddStep(BaseStep step)
         {
             this.steps.Add(step);
         }
-
-        public void RemoveStep(Step step)
+        //Se cambió para que concuerde con OCP explicado en BaseStep
+        public void RemoveStep(BaseStep step)
         {
             this.steps.Remove(step);
         }
-
+        /*
+        Como comentario, creemos que este método debería cambiar ya que solo permite la impresión en consola.
+        */
         public void PrintRecipe()
         {
             Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
-            foreach (Step step in this.steps)
+            foreach (BaseStep step in this.steps)
             {
-                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
-                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+               Console.WriteLine(step.GetTextToPrint());
             }
             Console.WriteLine( $"Costo de producción: {this.GetProductionCost()}");
         }
-        //Agregado por expert
+        /// <summary>
+        /// Al ser Recipe la única clase que conoce todos los steps ya que los tiene en una lista, 
+        /// por expert llegamos a la conclusión de que sería buena idea colocar aqui la responsabilidad de
+        /// calcular el costo de producción.
+        /// </summary>
+        /// <returns></returns>
         public double GetProductionCost()
         {
         double result = 0;
-        foreach (Step step in this.steps)
+        foreach (BaseStep step in this.steps)
             {
                 result = result + step.GetStepCost();
             }
--- src/Library/Step.cs
+++ src/Library/Step.cs
@@ -6,7 +6,7 @@
 
 namespace Full_GRASP_And_SOLID.Library
 {
-    public class Step
+    public class Step: BaseStep
     {
         public Step(Product input, double quantity, Equipment equipment, int time)
         {
@@ -23,12 +23,24 @@ namespace Full_GRASP_And_SOLID.Library
         public int Time { get; set; }
 
         public Equipment Equipment { get; set; }
-
-        public double GetStepCost()
+        /// <summary>
+        ///Por expert vimos necesario que la clase Step fuera la responsable de calcular su propio costo,
+        //Ya que es la que tiene toda la información necesaria para ello.
+        /// </summary>
+        /// <returns></returns>
+        public override double GetStepCost()
         {
-            return
-                (this.Input.UnitCost * this.Quantity) +
-                (this.Equipment.HourlyCost * this.Time);
+            return (this.Input.UnitCost * this.Quantity) + (this.Equipment.HourlyCost * this.Time);
+        }
+        /// <summary>
+        /// Agregamos este método para que el método PrintRecipe en Recipe pueda recibir textos distintos
+        /// dependiendo del tipo de Step.
+        /// </summary>
+        /// <returns></returns>
+        public override string GetTextToPrint()
+        {
+            return $"{this.Quantity} de '{this.Input.Description}' " +
+            $"usando '{this.Equipment.Description}' durante {this.Time}";
         }
     }
 }
